25bda6b29c66bbbe30bbad533689e0ed22d99d87
e.karasev <e.karasev@app-smart.ru>
2021-07-23 15:28:23 +0200

    first commit

---
+++ src/test/java/citrusdemohil/Test.java
@@ -0,0 +1,58 @@
+package citrusdemohil;
+
+import citrusdemohil.pojo.Data;
+import citrusdemohil.pojo.Support;
+import citrusdemohil.pojo.User;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.context.TestContext;
+import com.consol.citrus.dsl.testng.TestNGCitrusTestRunner;
+import com.consol.citrus.message.MessageType;
+import org.springframework.http.HttpStatus;
+
+public class Test extends TestNGCitrusTestRunner {
+
+    private TestContext context;
+
+    @org.testng.annotations.Test
+    @CitrusTest
+    public void getTestActions() {
+        this.context = citrus.createTestContext();
+
+        context.setVariable("value", "superValue");
+        echo("Property \"value\" = " + context.getVariable("value"));
+        echo("We have userId = " + context.getVariable("userId"));
+        echo("Property \"userId\" = " + "${userId}");
+
+        http(httpActionBuilder -> httpActionBuilder
+                .client("restClientReqres")
+                .send()
+                .get("api/users/${userId}")
+        );
+
+        http(httpActionBuilder -> httpActionBuilder
+                .client("restClientReqres")
+                .receive()
+                .response(HttpStatus.OK)
+                .messageType(MessageType.JSON)
+                .payload(getJsonData(), "objectMapper")
+        );
+    }
+
+    public User getJsonData() {
+        User user = new User();
+
+        Data data = new Data();
+        data.setId(Integer.valueOf(context.getVariable("userId")));
+        data.setEmail("janet.weaver@reqres.in");
+        data.setFirstName("Janet");
+        data.setLastName("Weaver");
+        data.setAvatar("https://reqres.in/img/faces/2-image.jpg");
+        user.setData(data);
+
+        Support support = new Support();
+        support.setUrl("https://reqres.in/#support-heading");
+        support.setText("To keep ReqRes free, contributions towards server costs are appreciated!");
+        user.setSupport(support);
+        return user;
+    }
+}
